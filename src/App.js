6d53699a2abfadd64e09e56cf7c5bf898779106f
Daniel Arango <d.arangoc2@uniandes.edu.co>
2023-09-22 23:57:48 -0500

    Ajustes finales

--- src/App.js
+++ src/App.js
@@ -6,66 +6,99 @@ import Form from 'react-bootstrap/Form';
 function App() {
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/;
-  const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"})
+  const [formValues, setFormValues] = useState({ email: '', password: '', favClass: '1' });
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
-  const handleEmailChange = ((e) => {
-    setFormValues({...formValues, email: e.target.value})
+  const handleEmailChange = (e) => {
+    setFormValues({ ...formValues, email: e.target.value });
     if (emailRegex.test(e.target.value)) {
-      e.target.setCustomValidity("");
+      setIsEmailValid(true);
+      e.target.setCustomValidity('');
     }
-  });
- 
-  const handlePasswordChange = ((e) => {
-    setFormValues({...formValues, password: e.target.value})
-    if (passwordRegex.test(e.target.value)) {
-      e.target.setCustomValidity("");
+    else {
+      setIsEmailValid(false);
+      e.target.setCustomValidity('Invalid email');
     }
-  });
- 
-  const handleSelectChange = ((e) => {
-    setFormValues({...formValues, favClass: e.target.value})
-  });
-  // Si se clickea el boton de submit se verifica que el email y el password sean validos
-  const clickSubmit = ((e) => {
-    e.preventDefault();
-    if (emailRegex.test(formValues.email) && passwordRegex.test(formValues.password)) {
-      alert(JSON.stringify(formValues))
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setFormValues({ ...formValues, password: newPassword });
+
+    if (passwordRegex.test(newPassword)) {
+      setIsPasswordValid(true);
+      e.target.setCustomValidity('');
     } else {
-      alert("Formulario no valido");
+      setIsPasswordValid(false);
+      e.target.setCustomValidity('Invalid password');
     }
-  });
+  };
+
+  const handleSelectChange = (e) => {
+    setFormValues({ ...formValues, favClass: e.target.value });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    if (emailRegex.test(formValues.email) && isPasswordValid && isEmailValid) {
+      alert(JSON.stringify(formValues));
+    } else {
+      alert('Formulario no válido');
+    }
+  };
+
   return (
     <div>
-     <h1>Ejemplo de formularios!</h1>
-    
-     <Form>
-     <Form.Group className="mb-6" controlId="formBasicEmail">
-       <Form.Label>Email address</Form.Label>
-       <Form.Control type="email" 
-       placeholder="Enter email"
-       onChange= {handleEmailChange} />
-       <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-     </Form.Group>
+      <h1>Ejemplo de formularios!</h1>
 
-     <Form.Group className="mb-3" controlId="formBasicPassword">
-       <Form.Label>Password</Form.Label>
-       <Form.Control type="password"
-        placeholder="Password"
-        onChange = {handlePasswordChange}/>
-       <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>
-     </Form.Group>
-     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-       <Form.Label>Favorite Class</Form.Label>
-       <Form.Select onChange = {handleSelectChange}>
-         <option value="1">ISIS3710</option>
-         <option value="2">Programación con tecnologias web</option>
-       </Form.Select>
-     </Form.Group>
-     <Button variant="primary" onClick={clickSubmit}>
-       Submit
-     </Button>
-   </Form>
-   </div>
+      <Form>
+        <Form.Group className="mb-6" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={handleEmailChange}
+            isInvalid={!isEmailValid}
+            isValid={isEmailValid}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Invalid Email
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+            isInvalid={!isPasswordValid}
+            isValid={isPasswordValid}
+          />
+          <Form.Text className="text-muted">
+            Your password should have numbers and letters and should be at least 9 characters long
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Invalid password
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Label>Favorite Class</Form.Label>
+          <Form.Select onChange={handleSelectChange}>
+            <option value="1">ISIS3710</option>
+            <option value="2">Programación con tecnologías web</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" onClick={clickSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 }
 
